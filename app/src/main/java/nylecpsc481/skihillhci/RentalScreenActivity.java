eb4e6e7532eb940d60fb531b97d94d7b44477f4d
Alex Thompson <alexcthomp@gmail.com>
2016-11-14 16:48:42 -0700

    Date picker spinner is working!

--- app/src/main/java/nylecpsc481/skihillhci/RentalScreenActivity.java
+++ app/src/main/java/nylecpsc481/skihillhci/RentalScreenActivity.java
@@ -2,6 +2,7 @@ package nylecpsc481.skihillhci;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
 
 public class RentalScreenActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class RentalScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rental_screen);
+
+        DatePicker rentalDatePicker = (DatePicker)findViewById(R.id.datePicker2);
+        rentalDatePicker.setSpinnersShown(true);
+        rentalDatePicker.setCalendarViewShown(false);
     }
 }
