994b67e96b79fbeb3cd17daa0a6b61b2b7351b94
Alex Thompson <alexcthomp@gmail.com>
2016-11-16 14:26:02 -0700

    Added condition markers for the condition toggle button. Also added logic to toggle these markers on and off.

--- app/src/main/java/nylecpsc481/skihillhci/MapActivity.java
+++ app/src/main/java/nylecpsc481/skihillhci/MapActivity.java
@@ -1,7 +1,10 @@
 package nylecpsc481.skihillhci;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.CompoundButton;
+        import android.widget.ToggleButton;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        final View conditions = findViewById(R.id.conditionsImage);
+        /*conditions toggle button logic*/
+        ToggleButton toggleConditions = (ToggleButton) findViewById(R.id.toggleConditions);
+        toggleConditions.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+
+                    conditions.setVisibility(View.VISIBLE);
+
+                } else {
+
+                    conditions.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
     }
+
+
 }
