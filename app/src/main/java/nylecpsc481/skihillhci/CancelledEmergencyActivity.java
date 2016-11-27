4f67bc81a94b539c376c1bf32dbe13d8c7dbde19
NyleVII <stephenantymis@gmail.com>
2016-11-26 20:48:08 -0700

    Emergency cancellation is now possible

--- app/src/main/java/nylecpsc481/skihillhci/CancelledEmergencyActivity.java
+++ app/src/main/java/nylecpsc481/skihillhci/CancelledEmergencyActivity.java
@@ -1,25 +1,21 @@
 package nylecpsc481.skihillhci;
 
 import android.content.Intent;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ProgressBar;
 
-public class EmergencyActivity extends AppCompatActivity {
-
+public class CancelledEmergencyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_emergency);
+        setContentView(R.layout.activity_cancelled_emergency);
     }
 
-    public void cancelEmergency(View view){
+    public void mainMenu(View view){
 
-        Intent intent = new Intent(this, CancelledEmergencyActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
 }
