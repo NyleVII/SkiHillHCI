086999b05219250ffc1e53d429d15a3ca21d3d26
martinkusch <martin.kusch@ucalgary.ca>
2016-11-27 21:12:51 -0700

    Update RentalScreenActivity.java

--- app/src/main/java/nylecpsc481/skihillhci/RentalScreenActivity.java
+++ app/src/main/java/nylecpsc481/skihillhci/RentalScreenActivity.java
@@ -16,19 +16,19 @@ public class RentalScreenActivity extends AppCompatActivity {
 
         /*Ski spinner*/
         Spinner skiSpinner = (Spinner)findViewById(R.id.skiSpinner);
-        String[] ski = new String[]{"0","1", "2", "3", "4", "5"};
+        String[] ski = new String[]{"0","1 ($15)", "2 ($30)", "3 ($45)", "4 ($60)", "5 ($75)"};
         ArrayAdapter<String> skiAdapt = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, ski);
         skiSpinner.setAdapter(skiAdapt);
 
         /*Snowboard spinner*/
         Spinner snowboardSpinner = (Spinner)findViewById(R.id.snowboardSpinner);
-        String[] snowboard = new String[]{"0","1", "2", "3", "4", "5"};
+        String[] snowboard = new String[]{"0","1 ($15)", "2 ($30)", "3 ($45)", "4 ($60)", "5 ($75)"};
         ArrayAdapter<String> snowboardAdapt = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, snowboard);
         snowboardSpinner.setAdapter(snowboardAdapt);
 
         /*Helmet spinner*/
         Spinner helmetSpinner = (Spinner)findViewById(R.id.helmetSpinner);
-        String[] helmet = new String[]{"0","1", "2", "3", "4", "5"};
+        String[] helmet = new String[]{"0","1 ($5)", "2 ($10)", "3 ($15)", "4 ($20)", "5 ($25)"};
         ArrayAdapter<String> helmetAdapt = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, helmet);
         helmetSpinner.setAdapter(helmetAdapt);
 
