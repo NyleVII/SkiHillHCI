b7602024cea8a465e5186d3787957be6253bc10e
NyleVII <stephenantymis@gmail.com>
2016-11-30 14:55:17 -0700

    Added 24 hour snow pack to main screen and change hill screen. Sunshine Village button works in change hill screen

--- app/src/main/java/nylecpsc481/skihillhci/ChangeHillActivity.java
+++ app/src/main/java/nylecpsc481/skihillhci/ChangeHillActivity.java
@@ -1,7 +1,9 @@
 package nylecpsc481.skihillhci;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChangeHillActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class ChangeHillActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_hill);
+    }
+
+    public void mainMenu(View view){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
